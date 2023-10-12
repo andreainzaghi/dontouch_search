e9473ae25f459dbf6b91a654f2723d77540fd5c9
andreainzaghi <andrea3aprile99@gmail.com>
2023-10-12 18:00:22 +0200

    css filter fix

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,22 +1,44 @@
 <template>
   <div class="flex_cards">
-    
     <!-- Navigation Buttons -->
     <div class="navigation-buttons">
       <button @click="goBack" :disabled="isBackDisabled">Back</button>
       <button @click="goForward" :disabled="isForwardDisabled">Forward</button>
     </div>
-    <button @click="showFilterDropdown = !showFilterDropdown">
-      Toggle Filters
-    </button>
 
     <div class="search-container">
       <!-- Search Box -->
       <div class="search-box">
-        <input class="search-input" v-model="searchQuery" placeholder="Search..." />
-        <button class="search-btn" @click="startSpeechRecognition">Voice Search</button>
+        <input
+          class="search-input"
+          v-model="searchQuery"
+          placeholder="Search..."
+        />
+        <div class="input_btn_search">
+          <button @click="showFilterDropdown = !showFilterDropdown">
+            FILTERS
+          </button>
+          <button class="search-btn" @click="startSpeechRecognition">
+          
+            <svg
+              width="30px"
+              height="30px"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19 10V12C19 15.866 15.866 19 12 19M5 10V12C5 15.866 8.13401 19 12 19M12 19V22M8 22H16M12 15C10.3431 15 9 13.6569 9 12V5C9 3.34315 10.3431 2 12 2C13.6569 2 15 3.34315 15 5V12C15 13.6569 13.6569 15 12 15Z"
+                stroke="#000000"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
-      
+
       <!-- Filters Dropdown -->
       <div class="filters-dropdown" v-show="showFilterDropdown">
         <div class="slide_show_on_off_filters">
@@ -27,64 +49,74 @@
               v-for="category in uniqueCategories"
               :key="category"
               @click="selectedCategory = category"
-              :class="{ 'active': selectedCategory === category }"
+              :class="{ active: selectedCategory === category }"
             >
               {{ category }}
             </button>
           </div>
 
-      <!-- Status Filter -->
-      <div class="filter-status">
-        <button @click="selectedStatus = ''">All Statuses</button>
-        <button
-          v-for="status in uniqueStatuses"
-          :key="status"
-          @click="selectedStatus = status"
-          :class="{ 'active': selectedStatus === status }"
-        >
-          {{ status }}
-        </button>
-      </div>
+          <!-- Status Filter -->
+          <div class="filter-status">
+            <button @click="selectedStatus = ''">All Statuses</button>
+            <button
+              v-for="status in uniqueStatuses"
+              :key="status"
+              @click="selectedStatus = status"
+              :class="{ active: selectedStatus === status }"
+            >
+              {{ status }}
+            </button>
+          </div>
 
-      <!-- Species Filter -->
-      <div class="filter-species">
-        <button @click="selectedSpecies = ''">All Species</button>
-        <button
-          v-for="species in uniqueSpecies"
-          :key="species"
-          @click="selectedSpecies = species"
-          :class="{ 'active': selectedSpecies === species }"
-        >
-          {{ species }}
-        </button>
-      </div>
+          <!-- Species Filter -->
+          <div class="filter-species">
+            <button @click="selectedSpecies = ''">All Species</button>
+            <button
+              v-for="species in uniqueSpecies"
+              :key="species"
+              @click="selectedSpecies = species"
+              :class="{ active: selectedSpecies === species }"
+            >
+              {{ species }}
+            </button>
+          </div>
 
-      <!-- Advanced Sorting -->
-      <div class="sort-container">
-        <select v-model="sortOption">
-          <option value="">Sort by...</option>
-          <option value="datePublished">Publication Date</option>
-          <option value="views">Views</option>
-          <option value="likes">Likes</option>
-          <option value="comments">Comments</option>
-        </select>
-      </div>
+          <!-- Advanced Sorting -->
+          <div class="sort-container">
+            <select v-model="sortOption">
+              <option value="">Sort by...</option>
+              <option value="datePublished">Publication Date</option>
+              <option value="views">Views</option>
+              <option value="likes">Likes</option>
+              <option value="comments">Comments</option>
+            </select>
+          </div>
 
-      <!-- Date Range Selector -->
-      <div class="date-range">
-        <input type="date" v-model="startDate" :min="minDate" :max="endDate" />
-        <input type="date" v-model="endDate" :min="startDate" :max="maxDate" />
+          <!-- Date Range Selector -->
+          <div class="date-range">
+            <input
+              type="date"
+              v-model="startDate"
+              :min="minDate"
+              :max="endDate"
+            />
+            <input
+              type="date"
+              v-model="endDate"
+              :min="startDate"
+              :max="maxDate"
+            />
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
     <!-- Cards Display -->
     <transition-group name="shuffle" tag="div" class="cards-container">
       <div v-for="item in paginatedData" :key="item.id" class="card">
         <img :src="getImagePath(item.img)" class="card-image" />
         <h2 class="card-title">{{ item.title }}</h2>
         <p class="card-description">{{ item.description }}</p>
-        
+
         <!-- Display statistics related to sorting -->
         <div class="stats">
           <span class="date-published">{{ item.datePublished }}</span>
@@ -145,16 +177,17 @@ export default {
     const endDate = ref("2023-12-31");
     const showFilterDropdown = ref(false);
 
-
     // Fetch data from backend when component is mounted
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://127.0.0.1:5000/data?page=${currentPage.value}&items_per_page=${itemsPerPage.value}&search_query=${searchQuery.value}&selected_category=${selectedCategory.value}&sort_option=${sortOption.value}`);
-        
+        const response = await fetch(
+          `http://127.0.0.1:5000/data?page=${currentPage.value}&items_per_page=${itemsPerPage.value}&search_query=${searchQuery.value}&selected_category=${selectedCategory.value}&sort_option=${sortOption.value}`
+        );
+
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
-        
+
         const jsonData = await response.json();
         data.value = jsonData;
       } catch (error) {
@@ -163,7 +196,19 @@ export default {
     };
 
     // Watchers to update data when a reactive property changes
-    watch([currentPage, searchQuery, selectedCategory, selectedStatus, selectedSpecies, sortOption, startDate, endDate], fetchData);
+    watch(
+      [
+        currentPage,
+        searchQuery,
+        selectedCategory,
+        selectedStatus,
+        selectedSpecies,
+        sortOption,
+        startDate,
+        endDate,
+      ],
+      fetchData
+    );
 
     // Ensure fetchData is called when the component is mounted
     onMounted(fetchData);
@@ -180,14 +225,11 @@ export default {
       }
     };
 
- 
-  
     const minDate = computed(() => {
       return data.value.reduce((acc, item) => {
         return item.datePublished < acc ? item.datePublished : acc;
       }, "9999-12-31");
     });
-
 
     const maxDate = computed(() => {
       return data.value.reduce((acc, item) => {
@@ -225,16 +267,24 @@ export default {
     });
     const getImagePath = (imageName: any) => {
       return require(`@/assets/img/${imageName}`);
-    }
+    };
     const sortedAndFilteredData = computed(() => {
       let result = data.value.filter((item) => {
-        const matchesCategory = !selectedCategory.value || item.category === selectedCategory.value;
-        const matchesStatus = !selectedStatus.value || item.status === selectedStatus.value;
-        const matchesSpecies = !selectedSpecies.value || item.species === selectedSpecies.value;
-        const matchesQuery = item.title.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
-          item.description.toLowerCase().includes(searchQuery.value.toLowerCase());
+        const matchesCategory =
+          !selectedCategory.value || item.category === selectedCategory.value;
+        const matchesStatus =
+          !selectedStatus.value || item.status === selectedStatus.value;
+        const matchesSpecies =
+          !selectedSpecies.value || item.species === selectedSpecies.value;
+        const matchesQuery =
+          item.title.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
+          item.description
+            .toLowerCase()
+            .includes(searchQuery.value.toLowerCase());
 
-        return matchesCategory && matchesQuery && matchesStatus && matchesSpecies;
+        return (
+          matchesCategory && matchesQuery && matchesStatus && matchesSpecies
+        );
       });
 
       if (sortOption.value && isSortKey(sortOption.value)) {
@@ -253,7 +303,7 @@ export default {
         );
       });
     });
-  
+
     const totalPages = computed(() => {
       return Math.ceil(sortedAndFilteredData.value.length / itemsPerPage.value);
     });
@@ -264,10 +314,10 @@ export default {
       return sortedAndFilteredData.value.slice(start, end);
     });
 
-
-
     const isBackDisabled = computed(() => currentPage.value === 1);
-    const isForwardDisabled = computed(() => currentPage.value === totalPages.value);
+    const isForwardDisabled = computed(
+      () => currentPage.value === totalPages.value
+    );
 
     return {
       searchQuery,
@@ -292,8 +342,7 @@ export default {
       goForward,
       isBackDisabled,
       isForwardDisabled,
-      showFilterDropdown
-
+      showFilterDropdown,
     };
   },
 };
@@ -316,13 +365,34 @@ export default {
   font-size: 0.85em;
   color: #555;
 }
+.input_btn_search {
+  display: flex;
+}
+
+.input_btn_search button:nth-child(2) {
+  border-left: 1px solid grey;
+  border-right: 1px solid grey;
+  height: 100%;
+  border-top-right-radius: 350px;
+  border-bottom-right-radius: 350px;
+  color: rgb(0, 0, 0);
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+.input_btn_search button:nth-child(1) {
+ 
+  height: 100%;
+ background-color: #fff;
+  color: rgb(0, 0, 0);
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
 select,
 input {
   margin-bottom: 10px;
   margin-right: 10px;
   padding: 5px;
   border-radius: 3px;
-  border: 1px solid #ccc;
 }
 body {
   background-color: #f4f4f4;
@@ -336,7 +406,6 @@ body {
 
 .date-range input[type="date"] {
   padding: 5px;
-  border: 1px solid #ccc;
   border-radius: 4px;
   background-color: #f9f9f9;
 }
@@ -441,7 +510,6 @@ body {
 
 /* Making the search bar resemble GitHub's */
 input {
-  border: 1px solid #d1d5da;
   border-radius: 6px;
   padding: 5px 12px;
   font-size: 14px;
@@ -450,9 +518,6 @@ input {
 }
 
 input:focus {
-  border-color: #0366d6;
-  box-shadow: 0 0 5px rgba(3, 102, 214, 0.3);
-  outline: none;
 }
 
 button {
@@ -514,7 +579,7 @@ select:hover {
 /* Base and Reset styles */
 body {
   background-color: #f4f4f4;
-  font-family: 'Arial', sans-serif;
+  font-family: "Arial", sans-serif;
 }
 
 button,
@@ -533,19 +598,23 @@ input {
 .search-box {
   display: flex;
   gap: 0.5rem;
+  border-radius: 70px;
+  height: 60px;
+  border: 1px solid grey;
 }
 
 .search-input {
   flex-grow: 1;
   padding: 10px 15px;
-  border-radius: 25px;
-  border: 2px solid #d1d5da;
-  transition: border-color 0.3s;
+  border-radius: 70px;
+  border: none;
+
+  height: 100%;
 }
 
-.search-input:focus {
+/* .search-input:focus {
   border-color: #0366d6;
-}
+} */
 
 .filters-btn {
   padding: 10px 15px;
