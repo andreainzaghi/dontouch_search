99ed15e2ff691fc065c6ffe2aad0b60e31221f66
andreainzaghi <andrea3aprile99@gmail.com>
2023-10-13 21:43:20 +0200

    direttiva lazy loading

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.directive('lazy', {
+  mounted(el) {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const img = entry.target;
+            const src = img.getAttribute('data-lazy');
+            
+            if (src) {  // verifica che src non sia null
+              img.setAttribute('src', src);
+              img.classList.add('loaded');  // opzionale: aggiungi una classe una volta caricato
+              observer.disconnect();
+            }
+          }
+          
+      });
+    });
+
+    observer.observe(el);
+  }
+});
+
+app.mount('#app');
